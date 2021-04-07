9a9f26b49acf5a318247392b15463bbca3b3020d
Cory Boudreaux <cboud31@gmail.com>
2021-04-07 08:53:54 -0500

    Contact Form, Contact Filter completed

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -2,16 +2,36 @@ import React, { useContext } from 'react';
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
 
   return (
     <>
-      {contacts.map((contact) => {
-        return <ContactItem key={contact.id} contact={contact} />;
-      })}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map((contact) => {
+              return (
+                <CSSTransition key={contact.id} timeout={500} classNames="item">
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              );
+            })
+          : contacts.map((contact) => {
+              return (
+                <CSSTransition key={contact.id} timeout={500} classNames="item">
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              );
+            })}
+      </TransitionGroup>
     </>
   );
 };
