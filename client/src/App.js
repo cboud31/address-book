330c6088b612cb3f8830d12133ea5bebcaa8dc7c
Cory Boudreaux <cboud31@gmail.com>
2021-04-07 09:56:17 -0500

    Auth Context, Register and Login forms

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,30 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import '../src/App.css';
 
-import { NavBar, Home, About } from './components';
+import { NavBar, Home, About, Register, Login } from './components';
 
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <>
-          <NavBar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <>
+            <NavBar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+              </Switch>
+            </div>
+          </>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,6 @@
 export { default as NavBar } from './NavBar';
+
 export { default as Home } from './pages/Home';
 export { default as About } from './pages/About';
+export { default as Register } from './auth/Register';
+export { default as Login } from './auth/Login';
