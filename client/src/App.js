905b36fae60b1ee44fa66eb573ee2956585b4ee3
Cory Boudreaux <cboud31@gmail.com>
2021-04-09 12:25:57 -0500

    Connect Contacts UI to back end

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import '../src/App.css';
 
 import { NavBar, Home, About, Register, Login, Alerts } from './components';
+import PrivateRoute from './routing/PrivateRoute';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
@@ -25,7 +26,7 @@ const App = () => {
               <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path="/" component={Home} />
+                  <PrivateRoute exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/login" component={Login} />
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,21 +2,26 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import AuthContext from '../context/auth/authContext';
+import ContactContext from '../context/contact/contactContext';
 
 import ContactsIcon from '@material-ui/icons/Contacts';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 const NavBar = ({ title, icon }) => {
   const authContext = useContext(AuthContext);
+  const contactContext = useContext(ContactContext);
 
   const { isAuthenticated, logout, user } = authContext;
+  const { clearContacts } = contactContext;
 
   const onLogout = () => {
     logout();
+    clearContacts();
   };
 
   const authLinks = (
     <>
+      <li>Welcome to ContactKeeper! </li>
       <li>
         <a onClick={onLogout} href="#!">
           <ExitToAppIcon /> <span className="hide-sm">Logout</span>
